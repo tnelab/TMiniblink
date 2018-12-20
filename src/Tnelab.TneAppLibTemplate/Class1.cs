da254ee9594011967ddb067d751daf1bcb15b57b
esrin <esrin@qq.com>
2018-12-21 01:32:48 +0800

    添加vs2017工具Tnelab.TneApptoolkit相关项目

---
+++ src/Tnelab.TneAppLibTemplate/Class1.cs
@@ -0,0 +1,12 @@
+﻿//tne://to_typescript
+using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+
+namespace $safeprojectname$
+{
+	public class Class1
+{
+}
+}
