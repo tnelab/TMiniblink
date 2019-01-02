1378296dd0d312f8da140459c55281feb937c4bd
esrin <esrin@qq.com>
2019-01-02 19:35:44 +0800

    美化测试窗口

--- src/Tnelab.TneForm.Test/UI/Script/Default.ts
+++ src/Tnelab.TneForm.Test/UI/Script/Default.ts
@@ -1,6 +1,23 @@
 ﻿///<reference path="../../../Tnelab.TneForm.Test.BLL/SimpleTestService.cs.ts"/>
 async function InvokeTest() {
-    var simple = await new BLL.SimpleTestService().Ready();
-    alert("hi");
-    
+    try {
+        var win = Tnelab.ThisForm.GetHtmlWindow();
+        alert(win);
+        //var simple = await new BLL.SimpleTestService().Ready();
+        var newForm = await new Tnelab.TneForm().Ready();
+        await (newForm.Url = "Tne://Tnelab.TneForm.Test/ui/default.html?cmd=测试");
+        await newForm.Show();
+        await (newForm.ShowInTaskBar = false);
+        //var t = await newForm.RunFunc(function () {
+        //    var win = window as any;
+        //    win.MiniWindow();
+        //    return "";
+        //});
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+async function showId() {
+    alert(Tnelab.ThisForm.TneMapNativeObjectId);
 }
